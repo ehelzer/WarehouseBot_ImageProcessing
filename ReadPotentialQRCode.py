776525423d3b4432a18731d376c17ea8ea66291e
ehelzer <ehelzer@unomaha.edu>
2018-08-31 02:05:13 +0000

    Add files via upload

--- ReadPotentialQRCode.py
+++ ReadPotentialQRCode.py
@@ -15,6 +15,26 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(14, GPIO.OUT) #Pi pin 14 connects to CEENBoT pin 14 (PD2)
 
+#Wait
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(2, GPIO.OUT) #output mode
+
+#PWM
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT) #output mode
+pwm = GPIO.PWM(18, 100) #PWM pin at 100Hz Frequency - necessary?
+
+#movement commands
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(14, GPIO.OUT) #output mode (LSB)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(15, GPIO.OUT) #output mode (MSB)
+
 WIDTH = 1280
 HEIGHT = 1024
 qr_found = False
@@ -34,9 +54,11 @@ black = pygame.Color(0, 0, 0)
 textcol = pygame.Color(255, 255, 0)
 screen.fill(black)
 
+#continuously take a picture
 while True:
 	#conditional if only needed to take one qr code image
 	#while( qr_found == False ):
+		
 		# TAKE A PHOTO
 		camera.start_preview()
 		sleep(2)
@@ -57,16 +79,20 @@ while True:
 			image.load()
 		codes = zbarlight.scan_codes('qrcode', image)
 		
-		if codes == None:				#image is not of readable type (not white and black qr code), tries to invert the image 
+		#cannot find qr code
+		if codes == None:				
 			image = Image.open(files)
-
+			
+			#converts image to black and white
 			image = image.convert('L')
-
+			
+			#inverts the image
 			image1 = ImageOps.invert(image)
 			image1.save('/home/pi/imageProcessing/potentialQRCode.png')
 			
 			files1 = '/home/pi/imageProcessing/potentialQRCode.png'
 			image1 = open(files1, 'rb')
+			#checks again for a qr code
 			with image1:
 				image1 = Image.open(image1)
 				image1.load()
@@ -75,6 +101,10 @@ while True:
 		if (codes != None):
 			qr_found = True
 			print "QR Code Found"
+			
+			#TODO: check if qr code matches 
+			#various row/column of warehouse
+			 
 			GPIO.output(14, GPIO.HIGH)
 			#if (codes == '[\'http://qrs.ly/bn6pa5j\']'):     # QR code 1
 				#GPIO.output(14, GPIO.HIGH)
@@ -93,5 +123,16 @@ urlCode = ', '.join(codes)
 webbrowser.open(urlCode, new=2)
 print('QR codes: %s' % codes)
 sleep(3)
+
+#_________			
+			#where to implement PWM?
+			dc = 0
+			pwm.start(dc)
+			for dc in range (50):
+				pwm.ChangeDutyCycle(dc) #necessary to change duty cycle?
+				time.sleep(0.002) #2ms 
+				
+			pwm.stop()
+#__________
 
 pygame.quit()
